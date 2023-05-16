7b782f85f46c0e0e1390c43b9de510e4774a23f0
Jason Kennelly <tootle_likes_code@hotmail.com>
2023-05-16 21:48:20 +0100

    Created the holidays version of the DateService.

---
+++ ttrpg_bot/date_impl/holidays_date_service.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import holidays
+from holidays import HolidayBase
+
+from ttrpg_bot.dates.bank_holiday import BankHoliday
+from ttrpg_bot.dates.date_service import DateService
+
+
+class HolidaysDateService(DateService):
+
+    def __init__(self, country: str = "GB", province: str | None = None):
+        self._bank_holidays: list[BankHoliday] = []
+        self._initialise(country, province)
+
+    def _initialise(self, country: str, province: str | None):
+        current_year: int = datetime.today().year
+        if province:
+            country_holidays: HolidayBase = holidays.country_holidays(country, years=current_year, subdiv=province)
+        else:
+            country_holidays: HolidayBase = holidays.country_holidays(country, years=current_year)
+
+        self._convert(country_holidays)
+
+    def _convert(self, country_holidays: HolidayBase):
+        for date in country_holidays:
+            name = country_holidays.get(date)
+            bank_holiday = BankHoliday(date, name)
+            self._bank_holidays.append(bank_holiday)
+
+    def get_holidays(self) -> list[BankHoliday]:
+        return self._bank_holidays
